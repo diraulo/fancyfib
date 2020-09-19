27e38da76914dbd9e089edbe7296d7db93510ef2
Raoul DIFFOUO <diraulo@gmail.com>
2020-09-19 14:51:51 +0200

    client: scaffold react client app

---
+++ client/src/OtherPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => {
+  return (
+    <div>
+      Nothing to see here <span role="img">😜</span>
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
